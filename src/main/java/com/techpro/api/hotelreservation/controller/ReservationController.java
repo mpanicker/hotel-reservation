d8eece8276e00f7750943b53ca14a758783796d1
mason <mason.gruener@utexas.edu>
2019-08-13 19:21:16 -0500

    complete hotel reservation json layout

--- src/main/java/com/techpro/api/hotelreservation/controller/ReservationController.java
+++ src/main/java/com/techpro/api/hotelreservation/controller/ReservationController.java
@@ -1,13 +1,13 @@
 package com.techpro.api.hotelreservation.controller;
 
+import com.mongodb.util.JSON;
 import com.techpro.api.hotelreservation.domain.Reservation;
 import com.techpro.api.hotelreservation.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -20,4 +20,29 @@ public class ReservationController {
         Reservation r = reservationService.getReservation(bookingNumber);
         return ResponseEntity.ok().body(r);
     }
+
+/*    @GetMapping("/reservation")
+    public List<?> getReservationByEmail(@RequestParam final String email){
+        List<Reservation> reservationList = reservationService.getReservationEmail(email);
+        return reservationList;
+    }*/
+    //change contract email_address
+
+    @PostMapping( "/reservation")
+    @ResponseBody
+    public Reservation createReservation(@RequestBody Reservation newReservation){
+        Reservation r = reservationService.createNewReservation(newReservation);
+        return r;
+    }
+
+    @PutMapping("/reservation/{bookingNumber}")
+    public void updateReservation(@PathVariable final String bookingNumber, @RequestBody Reservation newReservation){
+        reservationService.updateReservation(bookingNumber, newReservation);
+    }
+
+    @DeleteMapping("/reservation/{bookingNumber}")
+    public void deleteReservation(@PathVariable final String bookingNumber) {
+        reservationService.deleteReservation(bookingNumber);
+    }
+
 }
--- src/main/java/com/techpro/api/hotelreservation/domain/Reservation.java
+++ src/main/java/com/techpro/api/hotelreservation/domain/Reservation.java
@@ -3,6 +3,8 @@ package com.techpro.api.hotelreservation.domain;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.data.annotation.Id;
 
+import java.util.Map;
+
 /**
  * Created by manoj on 8/8/2019.
  */
@@ -14,17 +16,28 @@ public class Reservation {
 
     public String bookingNumber;
 
+    //Start of JSON format
+    public String check_in_date;
+    public String check_out_date;
     public Integer num_of_guest;
-
     public Integer num_of_rooms;
+    public Double reservation_total_price;
+    public String reservation_currency;
+    public Double reservation_tax;
+    public String payment_method;
+    public Map<String, String> hotel_details;
 
-    public String email;
+    public Map<String, String> payment_details;
+
+    public Map<String, String> room_details;
+
+    public Map<String, String> primary_guest_details;
+    //End of JSON format
 
 
-    public Reservation(String bookingNumber, Integer num_of_guest, Integer num_of_rooms, String email) {
+    public Reservation(String bookingNumber, Integer num_of_guest, Integer num_of_rooms) {
         this.num_of_guest = num_of_guest;
         this.num_of_rooms = num_of_rooms;
-        this.email = email;
         this.bookingNumber = bookingNumber;
     }
 
@@ -35,8 +48,8 @@ public class Reservation {
     @Override
     public String toString() {
         return String.format(
-                "Reservation[id=%s, Number of Rooms='%s', Number of Guests='%s', Email ='%s']",
-                id, num_of_rooms, num_of_guest, email);
+                "Reservation[id=%s, Number of Rooms='%s', Number of Guests='%s']",
+                id, num_of_rooms, num_of_guest);
     }
 
     public String getBookingNumber() {
@@ -45,6 +58,79 @@ public class Reservation {
 
     public void setBookingNumber(String bookingNumber) {
         this.bookingNumber = bookingNumber;
+    }
+
+    public String getCheck_in_date() {
+        return check_in_date;
+    }
+
+    public void setCheck_in_date(String check_in_date) {
+        this.check_in_date = check_in_date;
+    }
+
+    public String getCheck_out_date() {
+        return check_out_date;
+    }
+
+    public void setCheck_out_date(String check_out_date) {
+        this.check_out_date = check_out_date;
+    }
+
+    public Double getReservation_total_price() {
+        return reservation_total_price;
+    }
+
+    public void setReservation_total_price(Double reservation_total_price) {
+        this.reservation_total_price = reservation_total_price;
+    }
+
+    public String getReservation_currency() {
+        return reservation_currency;
+    }
+
+    public void setReservation_currency(String reservation_currency) {
+        this.reservation_currency = reservation_currency;
+    }
+
+    public Double getReservation_tax() {
+        return reservation_tax;
+    }
+
+    public void setReservation_tax(Double reservation_tax) {
+        this.reservation_tax = reservation_tax;
+    }
+
+    public String getPayment_method() {
+        return payment_method;
+    }
+
+    public void setPayment_method(String payment_method) {
+        this.payment_method = payment_method;
+    }
+
+    public Map<String, String> getHotel_details() {
+        return hotel_details;
+    }
+
+    //handled in ReservationService
+    public void setHotel_details(Map<String, String> hotel_details) {
+        this.hotel_details = hotel_details;
+    }
+
+    public Map<String, String> getPayment_details() {
+        return payment_details;
+    }
+
+    public void setPayment_details(Map<String, String> payment_details) {
+        this.payment_details = payment_details;
+    }
+
+    public Map<String, String> getRoom_details() {
+        return room_details;
+    }
+
+    public void setRoom_details(Map<String, String> room_details) {
+        this.room_details = room_details;
     }
 
     public Integer getNum_of_guest() {
@@ -63,11 +149,11 @@ public class Reservation {
         this.num_of_rooms = num_of_rooms;
     }
 
-    public String getEmail() {
-        return email;
+    public Map<String, String> getPrimary_guest_details() {
+        return primary_guest_details;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setPrimary_guest_details(Map<String, String> primary_guest_details) {
+        this.primary_guest_details = primary_guest_details;
     }
 }
